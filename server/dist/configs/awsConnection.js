c1b2abaeb1161e70835c57c30abb03f1dd58f354
AliDurul <alidrl26@gmail.com>
2025-03-22 14:54:38 +0200

    updated .env variable name

--- server/dist/configs/awsConnection.js
+++ server/dist/configs/awsConnection.js
@@ -5,7 +5,7 @@ const client_s3_1 = require("@aws-sdk/client-s3");
 const s3Client = new client_s3_1.S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     }
 });
